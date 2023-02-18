44b10eda3026736ea96b98be5ef7bf68069e7898
unknown <111450962+NotEvenIndian@users.noreply.github.com>
2023-02-18 19:57:14 +0100

    Added more settings to layout file

--- settings.py
+++ settings.py
@@ -25,68 +25,6 @@ class Local_setting(NamedTuple):
     xmlpath: str
 
 
-settings = {
-    "drawing_target": {
-        "Elements": {
-            "Terrain": Xml_setting("Terrain", "./RenderTerrain"),
-            "Forest": Xml_setting("Forest", "./RenderForest"),
-            "Buildings": Xml_setting("Buildings", "./RenderBuilding"),
-            "Roads": Xml_setting("Roads", "./RenderRoad"),
-            "Railways": Xml_setting("Railways", "./RenderRail")
-        },
-        "Rail lines": {
-            "Train": Xml_setting("Train", "./RenderRailTrain"),
-            "Tram": Xml_setting("Tram", "./RenderRailTram"),
-            "Metro": Xml_setting("Metro", "./RenderRailMetro"),
-            "Monorail": Xml_setting("Monorail", "./RenderRailMonorail"),
-            "Cable car": Xml_setting("Cable car", "./RenderRailCableCar")
-        },
-        "Overlay": {
-            "Grid": Xml_setting("Grid", "./RenderGrid"),
-            "Districts": Xml_setting("District names", "./RenderDistrictName"),
-            "Building names": Xml_setting("Building names", "./RenderBuildingName"),
-            "Map symbols": Xml_setting("Map symbols", "./RenderMapSymbol"),
-            "Road names": Xml_setting("Road names", "./RenderRoadName"),
-            "Park names": Xml_setting("Park names", "./RenderParkName")
-        }
-
-    },
-    "public_transport": {
-        "General": {
-            "transport_routes": Xml_setting("Public transport routes", "./RenderTransportRoute"),
-            "route_numbering": Xml_setting("Route numberigs", "./RenderTransport")
-        },
-        "Lines and stops": {
-            "Bus lines": Xml_setting("Bus lines", "./RouteMapConfig/RenderBusLine"),
-            "Bus stops": Xml_setting("Bus stops", "./RouteMapConfig/RenderBusStop"),
-            "Tram lines": Xml_setting("Tram lines", "./RouteMapConfig/RenderTramLine"),
-            "Tram stops": Xml_setting("Tram stops", "./RouteMapConfig/RenderTramStop"),
-            "Metro lines": Xml_setting("Metro lines", "./RouteMapConfig/RenderMetroLine"),
-            "Metro stops": Xml_setting("Metro stops", "./RouteMapConfig/RenderMetroStation"),
-            "Train lines": Xml_setting("Train lines", "./RouteMapConfig/RenderTrainLine"),
-            "Train stops": Xml_setting("Train stops", "./RouteMapConfig/RenderTrainStation"),
-            "Monorail lines": Xml_setting("Monorail lines", "./RouteMapConfig/RenderMonorailLine"),
-            "Monorail stops": Xml_setting("Monorail stops", "./RouteMapConfig/RenderMonorailStation"),
-            "Blimp lines": Xml_setting("Blimp lines", "./RouteMapConfig/RenderBlimpLine"),
-            "Blimp stops": Xml_setting("Blimp stops", "./RouteMapConfig/RenderBlimpStop"),
-            "Ferry lines": Xml_setting("Ferry lines", "./RouteMapConfig/RenderFerryLine"),
-            "Ferry stops": Xml_setting("Ferry stops", "./RouteMapConfig/RenderFerryHarbor")
-        },
-        "Misc": {
-            "merge_bus_tram": Xml_setting("Merge bus and tram stops nearby", "./RouteMapConfig/MergeBusTramStop"),
-            "merge_train_metro": Xml_setting("Merge train and metro stations nearby", "./RouteMapConfig/MergeTrainMetroStaion"),
-            "auto_color": Xml_setting("Auto coloring", "./RouteMapConfig/AutoColoring"),
-            "widen_line": Xml_setting("Widen line if paths share same segment", "./RouteMapConfig/WidenOnSharedLines"),
-            "detect_end_loops": Xml_setting("Detect end loops", "./RouteMapConfig/DetectEndLoop"),
-            "mark_one_way": Xml_setting("Mark one way routes", "./RouteMapConfig/MarkOneWayRoutes"),
-            "merged_numbering": Xml_setting("Merged route numberings", "./RouteMapConfig/UseMergedRouteNumberings")
-        }
-
-    }
-
-}
-
-
 class Settings():
     """Class handling the external xml settings file."""
 
